0d8fc4218af6c69023221e11b8719e2324dd8deb
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-09-14 19:57:29 +0800

    Implemented Main Input Textbox. Needs refactoring.

---
+++ Type/AutoComplete.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xofel.AutoComplete
+{
+    /// <summary>
+    /// Provides functionality to suggest strings based on a query, and complete a partial query up to its common prefix.
+    /// </summary>
+    public class AutoComplete
+    {
+        //Initialized once and used in hash-matching LCP algorithm.
+        private static HashSet<String> lcpHs;
+
+        //Stores list of valid strings.
+        private SortedSet<String> dictionary;
+
+        /// <summary>
+        /// Builds an AutoComplete object with a given dictionary of strings.
+        /// </summary>
+        /// <param name="dictionary">Array of valid strings to build into a dictionary.</param>
+        public AutoComplete(string[] dictionary)
+        {
+            lcpHs = new HashSet<String>();
+
+            this.dictionary = new SortedSet<String>();
+            Load(dictionary);
+        }
+
+        private void Load(string[] dictionary)
+        {
+            foreach (string s in dictionary)
+            {
+                this.dictionary.Add(s);
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of suggestions in the dictionary.
+        /// </summary>
+        public int DictionarySize
+        {
+            get
+            {
+                return dictionary.Count;
+            }
+        }
+
+        /// <summary>
+        /// Adds a new suggestion to the dictionary.
+        /// </summary>
+        /// <param name="suggestion">Suggestion to add.</param>
+        public void AddSuggestion(string suggestion)
+        {
+            dictionary.Add(suggestion);
+        }
+
+        /// <summary>
+        /// Determines if a suggestion exists in the dictionary.
+        /// </summary>
+        /// <param name="suggestion">Suggestion to test.</param>
+        /// <returns>A boolean</returns>
+        public bool ContainsSuggestion(string suggestion)
+        {
+            return (dictionary.Contains(suggestion));
+        }
+
+        /// <summary>
+        /// Removes an existing suggestion from the dictionary.
+        /// </summary>
+        /// <param name="suggestion">Suggestion to remove.</param>
+        public void RemoveSuggestion(string suggestion)
+        {
+            dictionary.Remove(suggestion);
+        }
+
+        /// <summary>
+        /// Gets an array of suggestions matching a query.
+        /// </summary>
+        /// <param name="query">Query to run.</param>
+        /// <returns>Array of suggestions with prefix of query.</returns>
+        public string[] GetSuggestions(string query)
+        {
+            SortedSet<String> results = QueryResultSet(query);
+            string[] suggestions = new string[results.Count];
+            results.CopyTo(suggestions);
+            return suggestions;
+        }
+
+        private SortedSet<string> QueryResultSet(string query)
+        {
+            var results = new SortedSet<String>();
+            foreach (string str in dictionary.Where(elem => elem.StartsWith(query)))
+            {
+                results.Add(str);
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Completes a partial query to the longest common prefix of the result set.
+        /// </summary>
+        /// <param name="query">Query to run.</param>
+        /// <returns>Remaining string to complete query.</returns>
+        public string CompleteToCommonPrefix(string query)
+        {
+            SortedSet<String> results = QueryResultSet(query);
+            if (results.Count == 0)
+            {
+                return "";
+            }
+            else if (results.Count == 1)
+            {
+                return (results.ElementAt(0).Substring(query.Length));
+            }
+            else
+            {
+                return (FindCommonPrefix(results).Substring(query.Length));
+            }
+        }
+
+        //Finds the common prefix of a set of strings.
+        private string FindCommonPrefix(SortedSet<String> results)
+        {
+            int lcpIndex = LCPIndex(results.ElementAt(0), results.ElementAt(1));
+            string substr = results.ElementAt(0).Substring(0, lcpIndex);
+            for (int i = 2; i < results.Count; i++)
+            {
+                int newLcpIndex = LCPIndex(substr, results.ElementAt(i));
+                if (newLcpIndex != lcpIndex)
+                {
+                    lcpIndex = newLcpIndex;
+                    substr = results.ElementAt(i).Substring(0, lcpIndex);
+                }
+            }
+            return (results.ElementAt(0).Substring(0, lcpIndex));
+        }
+
+        //Uses hashing and binary search to determine the longest common prefix of a and b.
+        private int LCPIndex(string a, string b)
+        {
+            lcpHs.Clear();
+
+            int low = 0;
+            int high = Math.Min(a.Length, b.Length);
+            int mid = 0;
+            int found = -1;
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                string subA = a.Substring(0, mid);
+                string subB = b.Substring(0, mid);
+
+                lcpHs.Add(subA);
+
+                if (lcpHs.Contains(subB))
+                {
+                    found = mid;
+                    low = (mid + 1);
+                }
+                else
+                {
+                    high = (mid - 1);
+                }
+
+                lcpHs.Clear();
+            }
+
+            return found;
+        }
+    }
+}
--- Type/MainWindow.xaml.cs
+++ Type/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Xofel.AutoComplete;
+
 namespace Type
 {
     /// <summary>
@@ -19,14 +21,59 @@ namespace Type
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string INPUT_WELCOME_TEXT = "Start typng...";
+
+        private AutoComplete ac;
+        private Boolean isShowingWelcomeText;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] commands = { "done", "clear", "archive", "undo", "edit" };
+            ac = new AutoComplete(commands);
+
+            ShowWelcomeText();
+        }
+
+        private void ShowWelcomeText()
+        {
+            if (!isShowingWelcomeText)
+            {
+                textBox1.Text = INPUT_WELCOME_TEXT;
+                textBox1.Foreground = Brushes.LightGray;
+            }
+            isShowingWelcomeText = true;
+        }
+
+        private void HideWelcomeText()
+        {
+            if (isShowingWelcomeText)
+            {
+                string changedText = textBox1.Text;
+                string input = changedText.Substring(changedText.Length - 1);
+                textBox1.Text = input;
+                textBox1.Select(input.Length, 0);
+                textBox1.Foreground = Brushes.Black;
+            }
+            isShowingWelcomeText = false;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBox1.Text.Trim() == "")
+            {
+                ShowWelcomeText();
+            }
+            else
+            {
+                HideWelcomeText();
+            }
         }
     }
 }
