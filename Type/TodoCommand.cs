8382a0075e08a05f8f80fde5113f63eff6cbfe1b
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-09-15 12:59:09 +0800

    Implemented TodoItem Message Passing.

---
+++ Type/TodoCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Type
+{
+    class TodoCommand
+    {
+        public string command { get; set; }
+        public TodoItem item { get; set; }
+    }
+}
