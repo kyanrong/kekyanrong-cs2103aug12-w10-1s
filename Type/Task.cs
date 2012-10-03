87e3d753bc35e376af75d743579e25bde38ffd0c
Yong Michael <wrong92@gmail.com>
2012-10-03 11:00:59 +0800

    minor changes

--- Type/Task.cs
+++ Type/Task.cs
@@ -8,89 +8,48 @@ namespace Type
 {
     class Task
     {
+        // Store's the user's raw input
         private string rawText;
 
+        // Standard Properties of all tasks
         private bool done;
         private bool archive;
 
+        // Parsed Properties
         private DateTime start;
         private DateTime end;
-
         private List<string> tags;
-
         private List<Tuple<string, int>> tokens;
+
+        // Constructor
+        public Task(string rawText)
+        {
+            // saves the rawText
+            // parsing should be idempotent
+            // re-parsing on the same rawText
+            // should return the same values
+            this.rawText = rawText;
+
+           // default values
+           this.done = false;
+           this.archive = false;
+
+           // parse the input
+
+        }
 
         public string RawText
         {
-            get
-            {
-                return rawText;
-            }
+            get { return rawText; }
         }
 
-        public bool Done
-        {
-            get
-            {
-                return done;
-            }
+        public bool Done { get; set; }
+        public bool Archive { get; set; }
 
-            set
-            {
-                done = value;
-            }
-        }
-
-        public bool Archive
-        {
-            get
-            {
-                return archive;
-            }
-
-            set
-            {
-                archive = value;
-            }
-        }
-
-        public DateTime Start
-        {
-            get
-            {
-                return start;
-            }
-        }
-
-        public DateTime End
-        {
-            get
-            {
-                return end;
-            }
-        }
-
-        public IList<string> Tags
-        {
-            get
-            {
-                return tags.AsReadOnly();
-            }
-        }
-
-        public IList<Tuple<string, int>> Tokens
-        {
-            get
-            {
-                return tokens.AsReadOnly();
-            }
-        }
-
-        public Task(string rawText)
-        {
-            // @michael TODO
-            
-
-        }
+        // only getters
+        public DateTime Start { get; }
+        public DateTime End { get; }
+        public IList<string> Tags { get; }
+        public IList<Tuple<string, int>> Tokens { get; }
     }
 }
